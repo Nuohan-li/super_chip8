00469b97e19971abd81603f3e6573063fe46c73e
Nuohan Li <n228li@uwaterloo.ca>
2023-07-03 12:56:42 -0400

    added SDL2 for windows

--- Makefile
+++ Makefile
@@ -1,8 +1,12 @@
+# all: main.c memory.c cpu.c debug.c
+# 	gcc main.c memory.c cpu.c debug.c
 
 # Detect the operating system
 ifeq ($(OS),Windows_NT)
     # windows
     EXECUTABLE = a.exe
+    INCLUDE = -I win64_sdl/include -L win64_sdl/lib
+    MINGWSDL = -lmingw32 -lSDL2main -lSDL2
 else
     UNAME_S := $(shell uname -s)
     ifeq ($(UNAME_S),Darwin)
@@ -14,6 +18,16 @@ else
     endif
 endif
 
+# Default target
+all: $(EXECUTABLE)
+
+# Source files
+SRCS = main.c memory.c cpu.c debug.c
+
+# Compiler settings
+CC = gcc
+CFLAGS = -Wall -Wextra
+
 # Build the executable
-all: main.c memory.c cpu.c debug.c
-	gcc -Wall -Wextra main.c memory.c cpu.c debug.c
+$(EXECUTABLE): $(SRCS)
+	gcc -Wall -Wextra $(INCLUDE) $(SRCS) -o $(EXECUTABLE) $(MINGWSDL)
--- main.c
+++ main.c
@@ -4,23 +4,61 @@
 #include "debug.h"
 
 #ifdef __linux__ 
-    int main(){
-        cpu cpu_ctx;
-        char* file_name = "GAMES/GAMES/CAVE.ch8";
-        dump_game_content(file_name);
-        test();
-        debugger(&cpu_ctx);
-        return 0;
+#include "win64_sdl/include/SDL2/SDL.h"
+int main(int argc, char *argv[]){
+    
+    SDL_Init(SDL_INIT_EVERYTHING);
+    SDL_Window *window = SDL_CreateWindow("SDL test", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 400, 400, SDL_WINDOW_ALLOW_HIGHDPI); 
+    if(window == NULL){
+        printf("window creation failed\n");
+        return 1;
     }
+    SDL_Event event;
+    while(1){
+        if(SDL_PollEvent(&event)){
+            if(event.type == SDL_QUIT){
+                break;
+            }
+        }
+    }
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+
+    cpu cpu_ctx;
+    char* file_name = "GAMES/GAMES/CAVE.ch8";
+    dump_game_content(file_name);
+    test();
+    debugger(&cpu_ctx);
+    return 0;
+} 
 #elif __WIN64__
-    int main(){
-        cpu cpu_ctx;
-        char* file_name = "GAMES/GAMES/CAVE.ch8";
-        dump_game_content(file_name);
-        test();
-        debugger(&cpu_ctx);
-        return 0;
-    } 
+#include "win64_sdl/include/SDL2/SDL.h"
+int main(int argc, char *argv[]){
+    
+    SDL_Init(SDL_INIT_EVERYTHING);
+    SDL_Window *window = SDL_CreateWindow("SDL test", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 400, 400, SDL_WINDOW_ALLOW_HIGHDPI); 
+    if(window == NULL){
+        printf("window creation failed\n");
+        return 1;
+    }
+    SDL_Event event;
+    while(1){
+        if(SDL_PollEvent(&event)){
+            if(event.type == SDL_QUIT){
+                break;
+            }
+        }
+    }
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+
+    cpu cpu_ctx;
+    char* file_name = "GAMES/GAMES/CAVE.ch8";
+    dump_game_content(file_name);
+    test();
+    debugger(&cpu_ctx);
+    return 0;
+} 
 #elif __APPLE__
     int main(){
         cpu cpu_ctx;
