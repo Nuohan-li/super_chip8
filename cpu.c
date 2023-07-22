7f693dc706daa1563525d9239f738928b8bfa57f
FolaFatola <FolaDavid_12@yahoo.com>
2023-07-22 16:54:07 -0400

    Corrected incorrect instruction implementation

--- cpu.c
+++ cpu.c
@@ -100,26 +100,37 @@ void execute_opcode(cpu *cpu_ctx, uint16_t opcode) {
         //my current change.
         case OP_6xkk:
             // Set Vx = kk
+            kk = (opcode & 0x00FF);
+            x = (opcode & 0x0F00) >> 8;
             cpu_ctx->V[x] = kk;
             break;
 
         case OP_7xkk:
             // Set Vx = Vx + kk
+            kk = (opcode & 0x00FF);
+            x = (opcode & 0x0F00) >> 8;
             cpu_ctx->V[x] += kk;
             break;
 
         case OP_8xy0:
             // Set Vx = Vy
+            kk = (opcode & 0x00F0);
+            x = (opcode & 0x0F00) >> 8;
+            y = (opcode & 0x00F0) >> 4;
             cpu_ctx->V[x] = cpu_ctx->V[y];
             break;
 
         case OP_8xy1:
             // Set Vx = Vx OR Vy
+            x = (opcode & 0x0F00) >> 8;
+            y = (opcode & 0x00F0) >> 4;
             cpu_ctx->V[x] = (cpu_ctx->V[x] | cpu_ctx->V[y]);
             break;
 
         case OP_8xy2:
             // Set Vx = Vx AND Vy
+            x = (opcode & 0x0F00) >> 8;
+            y = (opcode & 0x00F0) >> 4;
             cpu_ctx->V[x] = (cpu_ctx->V[x] & cpu_ctx->V[y]);
             break;
 
