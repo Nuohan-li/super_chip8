1a9c719e3bbec734bc9831734d1baf0db614c2f2
FolaFatola <FolaDavid_12@yahoo.com>
2023-09-02 20:37:31 -0400

    init_keyboard and get_key functions complete

--- input.c
+++ input.c
@@ -1,5 +1,22 @@
 #include "input.h"
 
 void init_keyboard(input *inp, char *sdl_keys){
-    return;
+    for(int i = 0; i < 16; i++){
+        inp->keys[i] = sdl_keys[i];
+        inp->key_state[i] = RELEASED;
+    }
+}
+
+int get_key(input *inp, char sdl_key){
+    int index = 0;
+    for(int i = 0; i < 16; i++){
+        if(inp->keys[i] = sdl_key){
+            index = i;
+            
+        }
+        break;
+    }
+
+    return index;
+
 }
